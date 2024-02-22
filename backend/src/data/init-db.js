5384e9aee77086be0c209bf535419b92e38cb0df
Andrew Meads <andrew.meads@auckland.ac.nz>
2024-02-22 14:04:59 +1300

    Added basic User schema

--- backend/src/data/init-db.js
+++ backend/src/data/init-db.js
@@ -2,12 +2,21 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
+import { User } from "./schema.js";
 
 await mongoose.connect(process.env.DB_URL);
 console.log("Connected to database!");
 console.log();
 
+// Clear database
+await User.deleteMany({});
+
 // TODO any DB init stuff
+const alice = new User({ username: "alice", contacts: [] });
+await alice.save();
+
+const bob = new User({ username: "bob", contacts: [] });
+await bob.save();
 
 // Disconnect when complete
 await mongoose.disconnect();
