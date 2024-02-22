902a9d0db0059835b50f2168e21adfcbdbb6531c
boxy8 <zou.emily@yahoo.co.nz>
2024-02-22 16:51:19 +1300

    fix: select contact

--- frontend/src/components/ContactCard.jsx
+++ frontend/src/components/ContactCard.jsx
@@ -3,11 +3,9 @@ import styles from "./ContactCard.module.css";
 
 const ContactCard = ({ contact, setSelectedContact }) => {
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={() => setSelectedContact(contact)}>
       <img src={contact.photoUrl || ""} alt={contact.name} />
       <h2>{contact.name || ""}</h2>
-      {/* <p>{contact.phoneNumber || ""}</p>
-      <p>{contact.funFact || ""}</p> */}
     </div>
   );
 };
