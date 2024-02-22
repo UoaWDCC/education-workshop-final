59a570bb4971e901b9725abf6b2370b4e539e9f8
Jason <jasonabc0626@gmail.com>
2024-02-22 15:14:25 +1300

    save

---
+++ frontend/src/components/AddContact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddContact = () => {
+  return (
+    <div>
+      <h3 className=''>Add Contact</h3>
+      <label>Name</label>
+      <input placeholder='name'/>
+    </div>
+  )
+}
+
+export default AddContact
