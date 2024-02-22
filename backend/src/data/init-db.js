81627dd81a41cbdb21f578a7d208ec2ffac7b405
Andrew Meads <andrew.meads@auckland.ac.nz>
2024-02-22 14:10:32 +1300

    Added dummy data

--- backend/src/data/init-db.js
+++ backend/src/data/init-db.js
@@ -12,10 +12,55 @@ console.log();
 await User.deleteMany({});
 
 // TODO any DB init stuff
-const alice = new User({ username: "alice", contacts: [] });
+const alice = new User({
+  username: "alice",
+  contacts: [
+    {
+      name: "Ash Ketchum",
+      phoneNumber: "+1234567890",
+      photoUrl: null,
+      funFact: "Ash has a goal to become a Pokémon Master."
+    },
+    {
+      name: "Misty Waterflower",
+      phoneNumber: "+0987654321",
+      photoUrl: null,
+      funFact: "Misty specializes in water-type Pokémon."
+    },
+    {
+      name: "Brock Slate",
+      phoneNumber: "+1123456789",
+      photoUrl: null,
+      funFact: "Brock dreams of becoming the greatest Pokémon breeder."
+    }
+  ]
+});
 await alice.save();
 
-const bob = new User({ username: "bob", contacts: [] });
+const bob = new User({
+  username: "bob",
+  contacts: [
+    {
+      name: "Rick Sanchez",
+      phoneNumber: "+15555555501",
+      photoUrl: null,
+      funFact: "Rick is known for his eccentric and ingenious inventions."
+    },
+    {
+      name: "Morty Smith",
+      phoneNumber: "+15555555502",
+      photoUrl: null,
+      funFact: "Morty often finds himself reluctantly involved in his grandfather's adventures."
+    },
+    {
+      name: "Summer Smith",
+      phoneNumber: "+15555555503",
+      photoUrl: null,
+      funFact:
+        "Summer is quick-witted and sometimes more aware than Morty of the risks in Rick's adventures."
+    }
+  ]
+});
 await bob.save();
 
 // Disconnect when complete
